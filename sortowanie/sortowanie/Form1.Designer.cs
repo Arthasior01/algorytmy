bbcec3006d1ce419b86e790f93539c173fe987d1
Arthasior01 <44493014+Arthasior01@users.noreply.github.com>
2019-10-23 19:29:53 +0200

    Add files via upload

---
+++ sortowanie/sortowanie/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace sortowanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_sb = new System.Windows.Forms.Button();
+            this.btn_si = new System.Windows.Forms.Button();
+            this.btn_ss = new System.Windows.Forms.Button();
+            this.btn_cs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_sb
+            // 
+            this.btn_sb.Location = new System.Drawing.Point(192, 34);
+            this.btn_sb.Name = "btn_sb";
+            this.btn_sb.Size = new System.Drawing.Size(142, 56);
+            this.btn_sb.TabIndex = 0;
+            this.btn_sb.Text = "Sortowanie_B";
+            this.btn_sb.UseVisualStyleBackColor = true;
+            this.btn_sb.Click += new System.EventHandler(this.btn_sb_Click);
+            // 
+            // btn_si
+            // 
+            this.btn_si.Location = new System.Drawing.Point(437, 34);
+            this.btn_si.Name = "btn_si";
+            this.btn_si.Size = new System.Drawing.Size(142, 55);
+            this.btn_si.TabIndex = 1;
+            this.btn_si.Text = "Sortowanie_I";
+            this.btn_si.UseVisualStyleBackColor = true;
+            this.btn_si.Click += new System.EventHandler(this.btn_si_Click);
+            // 
+            // btn_ss
+            // 
+            this.btn_ss.Location = new System.Drawing.Point(192, 147);
+            this.btn_ss.Name = "btn_ss";
+            this.btn_ss.Size = new System.Drawing.Size(142, 55);
+            this.btn_ss.TabIndex = 2;
+            this.btn_ss.Text = "Sortowanie_S";
+            this.btn_ss.UseVisualStyleBackColor = true;
+            this.btn_ss.Click += new System.EventHandler(this.btn_ss_Click);
+            // 
+            // btn_cs
+            // 
+            this.btn_cs.Location = new System.Drawing.Point(437, 147);
+            this.btn_cs.Name = "btn_cs";
+            this.btn_cs.Size = new System.Drawing.Size(142, 54);
+            this.btn_cs.TabIndex = 3;
+            this.btn_cs.Text = "Sortowanie_C";
+            this.btn_cs.UseVisualStyleBackColor = true;
+            this.btn_cs.Click += new System.EventHandler(this.btn_cs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_cs);
+            this.Controls.Add(this.btn_ss);
+            this.Controls.Add(this.btn_si);
+            this.Controls.Add(this.btn_sb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_sb;
+        private System.Windows.Forms.Button btn_si;
+        private System.Windows.Forms.Button btn_ss;
+        private System.Windows.Forms.Button btn_cs;
+    }
+}
+
---
+++ sortowanie/sortowanie/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortowanie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sb_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 1, 5, 2, 3, 4, 5, 1, 2, 3, 1, 23, 4, 5, 2, 2, 12, 3, 6, 8, 3, 32, 12, 312, 6, 21, 90 };
+            MessageBox.Show(string.Join(" ", tab));
+
+            tab = SortowanieB(tab);
+
+            MessageBox.Show(string.Join(" ", tab));
+
+        }
+        int[]SortowanieB(int[] tab)
+        {
+            bool czyBylaZmiana = false;
+            do
+            {
+                czyBylaZmiana = false;
+                for (int i = 0; i < tab.Length - 1; i++)
+                {
+                    if (tab[i] > tab[i + 1])
+                    {
+                        czyBylaZmiana = true;
+                        int tmp = tab[i];
+                        tab[i] = tab[i + 1];
+                        tab[i + 1] = tmp;
+                    }
+                }
+            } while (czyBylaZmiana);
+
+            return tab;
+        }
+
+        private void btn_si_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 1, 1,412,3,12,51,23,1,51,2,31,25,12,4,2326,3,7,32,431,4,16,347,3,3,4,2,534,7,3,23,42,362,2734};
+            MessageBox.Show(string.Join(" ", tab));
+
+            tab = SortowanieI(tab);
+
+            MessageBox.Show(string.Join(" ", tab));
+        }
+        int[] SortowanieI(int[] tab)
+        {
+            int x = tab.Length;
+            for(int i = 0; i < x-1; i++)
+            {
+                int klucz = tab[i];
+                int j = i - 1;
+                while (j >=0 && tab[j] > klucz)
+                {
+                    tab[j + 1] = tab[j];
+                    j = j - 1;
+                }
+                tab[j + 1] = klucz;
+
+            }
+
+            return tab;
+        }
+
+        private void btn_ss_Click(object sender, EventArgs e)
+        {
+            int[] tab = {12,32,321,3124,123,124,123,123,125,125,121,212,2,1,2,3,5,2,1,2,45,6,9,5,3,2,6,7,9,5,2};
+            MessageBox.Show(string.Join(" ", tab));
+
+            tab = SortowanieS(tab);
+
+            MessageBox.Show(string.Join(" ", tab));
+        }
+        int[] SortowanieS(int[] tab)
+        {
+            int x = tab.Length;
+            for( int i = 0; i < x-1; i++)
+            {
+                int min = i;
+                for(int j = i + 1; j < x; j++)
+                {
+                    if(tab[j]<tab[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = tab[min];
+                tab[min] = tab[i];
+                tab[i] = temp;
+            }
+
+            return tab;
+        }
+
+        private void btn_cs_Click(object sender, EventArgs e)
+        {
+            int[] tab = {1,2,4,2,1,8,3,343,6,23,6,3,223,8,432,43,12,53,2,3,5,7,54 };
+            MessageBox.Show(string.Join(" ", tab));
+
+            tab = SortowanieS(tab);
+
+            MessageBox.Show(string.Join(" ", tab));
+        }
+        int[] SortowanieC(int[] tab)
+        {
+            int[] Sorttab = new int[tab.Length];
+
+            int min = tab[0];
+            int max = tab[0];
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] < min)
+                {
+                    min = tab[i];
+                }
+                else
+                if (tab[i] > max)
+                {
+                    max = tab[i];
+                }
+            }
+            int[] counts = new int[max - min + 1];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                counts[tab[i] - min]++;
+
+            }
+            counts[0]--;
+            for (int i = 1; i < tab.Length; i++)
+            {
+                counts[i] = counts[i] + counts[i - 1];
+            }
+            for (int i = tab.Length - 1; i >= 0; i--)
+            {
+                Sorttab[counts[tab[i] - min]--] = tab[i];
+            }
+            return Sorttab;
+        }
+    }
+}
